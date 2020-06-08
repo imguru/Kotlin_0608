9f6a11741694be841748b0df145092f5d2e888e8
Chansik Yun <chansigi@gmail.com>
2020-06-08 09:07:52 +0900

    .

---
+++ src/1_시작하기.kt
@@ -0,0 +1,17 @@
+// 1_시작하기
+
+// 1) .kt 확장자를 가집니다.
+// 2) public class를 위한 별도의 파일 생성이 필요하지 않습니다.
+
+/*
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java");
+    }
+}
+*/
+
+// 3) 전역 함수를 허용합니다.
+fun main() {
+    println("Hello, Kotlin")
+}
---
+++ src/com/lge/ex1/Sample.java
@@ -0,0 +1,7 @@
+package com.lge.ex1;
+
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java");
+    }
+}
