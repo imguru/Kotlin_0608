b79245eab172716637f70b3fe63d6be2ec5fd1e6
Chansik Yun <chansigi@gmail.com>
2020-06-08 14:51:33 +0900

    .

---
+++ src/6_클래스문법고급2.kt
@@ -0,0 +1,65 @@
+package ex6_2
+
+import java.io.*
+
+interface State : Serializable
+
+interface View {
+    fun getCurrentState(): State
+    fun restoreState(state: State)
+}
+
+class Button(var x: Int, var y: Int) : View {
+
+    // Nested Class가 기본입니다.
+    // 내부 클래스를 만드는 키워드 - inner
+    class ButtonState(val x: Int, val y: Int) : State
+
+    override fun getCurrentState(): State {
+        return ButtonState(x, y)
+    }
+
+    override fun restoreState(state: State) {
+        state as ButtonState
+
+        x = state.x
+        y = state.y
+    }
+
+    override fun toString(): String {
+        return "Button(x=$x, y=$y)"
+    }
+}
+
+fun main() {
+    val button = Button(10, 20)
+
+    val fos = FileOutputStream("button2.dat")
+    val oos = ObjectOutputStream(fos)
+    oos.writeObject(button.getCurrentState())
+    fos.close()
+    oos.close()
+
+    button.x = 30
+    button.y = 40
+
+    val fis = FileInputStream("button2.dat")
+    val ois = ObjectInputStream(fis)
+    val s = ois.readObject() as State
+
+    button.restoreState(s)
+    println(button)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/com/lge/ex6/Sample.java
+++ src/com/lge/ex6/Sample.java
@@ -1,5 +1,88 @@
 package com.lge.ex6;
 
+import java.io.*;
+
+interface State extends Serializable {
+}
+
+interface View {
+    State getCurrentState();
+
+    void restoreState(State state);
+}
+
+class Button implements View {
+    private int x;
+    private int y;
+
+    public Button(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Button{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    // java.io.NotSerializableException: com.lge.ex6.Button
+    // Inner Class(내부 클래스) - default
+    //  : 외부 클래스에 대한 참조가 암묵적으로 존재한다.
+    // Nested Class(중첩 클래스) - static
+    //  : 외부 클래스에 대한 참조가 암묵적으로 존재하지 않는다.
+    static class ButtonState implements State {
+        int x;
+        int y;
+
+        ButtonState(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    @Override
+    public State getCurrentState() {
+        return new ButtonState(x, y);
+    }
+
+    @Override
+    public void restoreState(State state) {
+        ButtonState s = (ButtonState) state;
+        this.x = s.x;
+        this.y = s.y;
+    }
+}
+
+
+public class Sample {
+    public static void main(String[] args) throws Exception {
+        Button button = new Button(30, 40);
+
+        FileInputStream fis = new FileInputStream("button.dat");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+
+        State s = (State) ois.readObject();
+        button.restoreState(s);
+
+        System.out.println(button);
+
+        /*
+        Button button = new Button(10, 20);
+
+        State s = button.getCurrentState();
+
+        FileOutputStream fos = new FileOutputStream("button.dat");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(s);
+        */
+
+    }
+}
+
+/*
 interface Clickable {
     void click();
 
@@ -23,3 +106,4 @@ public class Sample {
 
     }
 }
+*/
