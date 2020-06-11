a4e64ac55f312733d93ac03ebadb2d891c4beb3c
Chansik Yun <chansigi@gmail.com>
2020-06-11 12:00:38 +0900

    .

---
+++ android/FirstApp/app/src/main/java/com/lge/firstApp/MainActivity2.kt
@@ -0,0 +1,15 @@
+package com.lge.firstApp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+// styles.xml
+// <style name="AppTheme" parent="Theme.AppCompat.Light.NoActionBar">
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
