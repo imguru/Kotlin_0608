9c8359d5c52aacae290b47953a4cff5b8ea4f403
Chansik Yun <chansigi@gmail.com>
2020-06-11 14:41:50 +0900

    .

--- android/FirstApp/app/src/main/java/com/lge/firstApp/MainActivity4.kt
+++ android/FirstApp/app/src/main/java/com/lge/firstApp/MainActivity4.kt
@@ -9,3 +9,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main3)
     }
 }
+
+// MDC - Material Design Component
+//   https://github.com/material-components/material-components-android
+
+// 1. app수준의 build.gradle 의존성에
+//  implementation 'com.google.android.material:material:<version>'
+
+// 2. Theme 적용(styles.xml)
+
+
+// https://medium.com/androiddevelopers/migrating-to-material-components-for-android-ec6757795351
