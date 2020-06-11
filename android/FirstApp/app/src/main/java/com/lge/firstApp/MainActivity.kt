c7fd06001d79832be1c2e5cd092235a46651c71c
Chansik Yun <chansigi@gmail.com>
2020-06-11 10:01:11 +0900

    .

---
+++ android/FirstApp/app/src/main/java/com/lge/firstApp/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.lge.firstApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// 코틀린 기반 안드로이드 프로젝트
+//  - Gradle
+
+// Project             : build.gradle (프로젝트 수준 build.gradle)
+//   - Module(app)     : build.gradle (모듈 수준 build.gradle)
+
+// project 수준 build.gradle
+// 1. ext.kotlin_version = "1.3.72"
+// 2. classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+
+// app 수준 build.gradle
+//  1. apply plugin: 'kotlin-android'
+//  2. apply plugin: 'kotlin-android-extensions'
+
+//  3. dependencies
+//     - implementation "org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version"
+//     - implementation 'androidx.core:core-ktx:1.3.0'
+
+// Android에서 Java8을 사용하는 방법
+// compileOptions {
+//        sourceCompatibility JavaVersion.VERSION_1_8
+//        targetCompatibility JavaVersion.VERSION_1_8
+// }
+
+// Kotlin이 Java8을 사용하는 방법
+// kotlinOptions {
+//        jvmTarget = JavaVersion.VERSION_1_8.toString()
+// }
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
