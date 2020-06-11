287cd1d04f87418e2ff0f4f1d1ce58b83de1faa3
Chansik Yun <chansigi@gmail.com>
2020-06-11 13:46:22 +0900

    .

--- android/FirstApp/app/src/main/java/com/lge/firstApp/MainActivity3.kt
+++ android/FirstApp/app/src/main/java/com/lge/firstApp/MainActivity3.kt
@@ -1,25 +1,73 @@
 package com.lge.firstApp
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
+import kotlinx.android.synthetic.main.fragment_main.button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        val fragment = MainFragment2()
-
-        /*
-        val fragmentManager = supportFragmentManager
-        fragmentManager.beginTransaction()
-            .replace(R.id.mainFrame, fragment)
-            .commit()
-        */
-
+        val fragment = FirstFragment()
         supportFragmentManager.commit {
             replace(R.id.mainFrame, fragment)
+        }
+    }
+}
+
+class FirstFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_main, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button.text = "First"
+        button.setOnClickListener {
+
+        }
+    }
+}
+
+class SecondFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_main, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button.text = "Second"
+        button.setOnClickListener {
+
+        }
+    }
+}
+
+class ThirdFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_main, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button.text = "Third"
+        button.setOnClickListener {
+
         }
     }
 }
