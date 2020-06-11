686ee4a7760c0e907c5785c1b91e849fa4d0b593
Chansik Yun <chansigi@gmail.com>
2020-06-11 14:33:11 +0900

    .

---
+++ android/FirstApp/app/src/main/java/com/lge/firstApp/MainActivity4.kt
@@ -0,0 +1,11 @@
+package com.lge.firstApp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+    }
+}
