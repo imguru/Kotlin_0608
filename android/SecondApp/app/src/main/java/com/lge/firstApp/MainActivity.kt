334f5e149e0b0854b9e7d9752f72b0101a453734
Chansik Yun <chansigi@gmail.com>
2020-06-11 15:27:09 +0900

    .

--- android/SecondApp/app/src/main/java/com/lge/firstApp/MainActivity.kt
+++ android/SecondApp/app/src/main/java/com/lge/firstApp/MainActivity.kt
@@ -1,12 +1,50 @@
 package com.lge.firstApp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            okhttp3Version1()
+        }
+    }
+
+    private fun okhttp3Version1() {
+        // 1. OkHttpClient 객체 생성
+        val client = OkHttpClient()
+
+        // 2. Request - GET
+        val request = Request.Builder().apply {
+            url("https://api.github.com/users/JakeWharton")
+        }.build()
+
+        // 3. Call 객체 생성
+        //  : 동기 또는 비동기
+        //  => 사용자가 동기 호출 또는 비동기 호출을 결정할 수 있다.
+        val call = client.newCall(request)
+
+        // 4. 동기
+        val response = call.execute()
+
+        // 5. 서버의 응답 코드(statusCode)를 통해 요청의 성공 실패를 판단해야 한다.
+        // 200 ~ 299 = OK
+        // 400 ~ 499 = Client Error
+        // 500 ~ 599 = Server Error
+
+        // if (response.code in 200..299) {
+        if (response.isSuccessful) {
+            val body = response.body
+            val json = body.toString()
+            Log.e("XXX", "Response: $json")
+        }
     }
 }
 
@@ -16,12 +54,11 @@ class MainActivity : AppCompatActivity() {
 //    implementation 'com.squareup.okhttp3:okhttp:4.7.2'
 
 
-
 // Server 연동
 //    - Github API v3 - REST API(JSON)
 //    - Github API v4 - GraphQL  X
 //   : https://github.com/JakeWharton
-//    => api.github.com/users/JakeWharton
+//    => https://api.github.com/users/JakeWharton
 /*
 {
   "login": "JakeWharton",
