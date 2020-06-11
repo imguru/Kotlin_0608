11bffbed2a543845588e36a8994e41e59d15c101
Chansik Yun <chansigi@gmail.com>
2020-06-11 13:18:51 +0900

    .

--- android/FirstApp/app/src/main/java/com/lge/firstApp/MainActivity2.kt
+++ android/FirstApp/app/src/main/java/com/lge/firstApp/MainActivity2.kt
@@ -1,7 +1,11 @@
 package com.lge.firstApp
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
 // 1.
 // styles.xml
@@ -15,5 +19,38 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val fragment = MainFragment()
+
+        val fragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction()
+            .replace(R.id.mainFrame, fragment)
+            .commit()
     }
 }
+
+// 3. Fragment
+// MainFragment.kt
+class MainFragment : Fragment() {
+    // Inflating - XML
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
