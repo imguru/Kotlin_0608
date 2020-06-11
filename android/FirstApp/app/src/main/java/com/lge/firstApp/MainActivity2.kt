3b860be66466688f22b537099b5b1e6326d61e10
Chansik Yun <chansigi@gmail.com>
2020-06-11 13:10:25 +0900

    .

--- android/FirstApp/app/src/main/java/com/lge/firstApp/MainActivity2.kt
+++ android/FirstApp/app/src/main/java/com/lge/firstApp/MainActivity2.kt
@@ -3,11 +3,15 @@ package com.lge.firstApp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+// 1.
 // styles.xml
 // <style name="AppTheme" parent="Theme.AppCompat.Light.NoActionBar">
 
-class MainActivity : AppCompatActivity() {
+// 2. activity_main2
+//     toolbar
+//     frameLayout - Fragment
 
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
